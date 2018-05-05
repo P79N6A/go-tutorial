953d2afe337885c1ef2b785371e95e09cfc65206
雷威 <leiwei2094@gmail.com>
2018-05-05 15:46:17 +0800

    commit

--- map/map_test.go
+++ map/map_test.go
@@ -14,4 +14,58 @@ func Test_create_map(t *testing.T)  {
 	dict2["leo"] = 30
 	fmt.Println(dict2["tom"])
 	fmt.Println(dict2["leo"])
+
+
+	dict3 := map [int] []string {}
+	dict3[1] = []string{"hello","world hhh"}
+
+	fmt.Println(dict3)
 }
+
+func Test_create_nil_map(t *testing.T)  {
+	// 这种方式创建的map是nil，如果直接操作它，会抛异常
+	var dict map[int]string
+
+	dict[1] = "hello"
+}
+
+func Test_create_nil_map2(t *testing.T)  {
+	colors := map[string]string{"red":"100","yellow":"200"}
+
+	value, exists := colors["red"]
+	if exists {
+		fmt.Println(value)
+	}
+}
+
+func Test_iterate_map(t *testing.T)  {
+	colors := createStringStringMap()
+
+	for key,value := range colors{
+		fmt.Printf("key: %s  value: %s\n",key,value)
+	}
+}
+
+func createStringStringMap() map[string]string {
+	colors := map[string]string{
+		"Blue" : "#f0f8ff",
+		"Coral": "#ff7F50",
+		"DarkGray": "#a9a9a9",
+		"Green": "#228b22",
+	}
+	return colors
+}
+
+
+func Test_delete_from_map(t *testing.T)  {
+	colors := createStringStringMap()
+	delete(colors, "Blue")
+	printMap(colors)
+}
+
+func printMap(m map[string]string)  {
+	for key,value := range m{
+		fmt.Printf("key: %s  value: %s\n",key,value)
+	}
+}
+
